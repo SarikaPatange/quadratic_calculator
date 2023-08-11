586fc36e02b25b9c29fd993449bcf091c708787c
Sarika Patange <128057504+SarikaPatange@users.noreply.github.com>
2023-08-11 00:30:47 +0000

    updating files

--- app.rb
+++ app.rb
@@ -7,3 +7,17 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/quadratic_calculator") do
+  erb(:quadratic_formula_calculator)
+end
+get("/quadratic_results") do
+  @a = params.fetch("coefficient_a")
+  @b = params.fetch("coefficient_b")
+  @c = params.fetch("coefficient_c")
+  
+  @answer = @a*x**2 + @b*x + @c
+  
+ 
+  erb(:quadratic_formula_results)
+end
